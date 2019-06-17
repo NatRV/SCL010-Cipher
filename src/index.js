125c511365f8a3d9058acbc87634d05179353fdb
NatRV <n.riquelme.veliz@gmail.com>
2019-06-17 17:26:04 -0400

    Actualizando estructura html, definiendo interacción, aplicando un poco de CSS

--- src/index.js
+++ src/index.js
@@ -1,64 +1,36 @@
-/*Acá va tu código*/
+//**********LOGICA Y ORDEN PARA QUE FUNCIONE EN CONSOLA(sólo funciona en consola):
+// let input = 'HOLA HOLA HOLA';
+// let offSet= 1;
+// let palabraCifrada = '';
 
-//charCodeAt(6);
-//String.fromCharCode(65,66,67)
-//console.log(String.charCodeAt(76));
-//
-//
-//
-//
-//window.onload es decirle a la página que cuando cargue desencadeneaón
-//metodoaddeventlistener(nuevo en html5)
-//let name = "Marcia";
-// name.lenght
+// for (let i=0; i<input.length; i++){
+//     let posicionInicialLetras = input[i].charCodeAt(0);
+//     let posicionFInalLetras = (posicionInicialLetras - 65 + offSet)% 26 + 65;
+//     let letraCifrada = String.fromCharCode(posicionFInalLetras);
 
-// let nameSeparado = name.split ("");
-// nameSeparado
-
-// nameSeparado.toUpperCase();
-// //
-// const btnSave =document.getElementById ("save"); //porque siempre va a ser boton, guardar botn en una variable
-// //console.log(btnSave) , para verificar sihay error en la consola
-
-// //llamar a mi funcion con el escuchador (forma1)
-// btnSave.addEventListener("click", () => { //Función del botón click, luego del click, colocar lo que yo quiero que pase
-//     //colocar getelement
-// })
-
-
-// //llamar a mi funcion con el escuchador (forma2)
-// function hello (){ //forma dellamar funcion1
-//     alert("hola")
+//     palabraCifrada = palabraCifrada + letraCifrada;
 // }
-// btnSave.addEventListener("click", hello);
+// console.log(palabraCifrada); //imprimir si funciona
 
-// //otra forma de declarar funcion
-// const btnSave = document.getElementById
-// ("save");
-// btnSave.addEventListener("click", hello);
-
-// btnSave.addEventListener("click", ()=> {
-//     let saveName = document.getElementById
-//     ("name").value;
-//     //console.log(saveName);
-//     let nameMayuscula = saveName.toUpperCase ();
-//     //console.log(nameMayuscula);
-// for(let i = 0; i < nameMayuscula.length; i++)    //la i es el índice, length es para recorrer toda la longitus
-// console.log(nameMayuscula [i]);
-// })
-let btnCode = document.getElementById('encrypt'); // se declara la variable del botón
-//console.log("encrypt"); // probar con consola
-
-
-//función botón "enviar" mensaje, es un boton falso que mostrará un aviso
-//function sendMssge(){
-//    document.getElementById('scrnNotice').style.display= 'block';
-//    document.getElementById('scrnPrincipal').style.display= 'none';
-//también ocultar title, h1,h2/ agregar divs
-//}
-
+//btn encriptar y que muestre en textarea
+let btnEncode= document.getElementById('encodeM');
+btnEncode.addEventListener('click', () => {
+  document.getElementById('message_out').innerHTML = `El resultado es: ${"message_in"}`; // colocar variable *textocifrado*
+})
+//btn desencriptar y que muestre en textarea
+let btnDecode= document.getElementById('decodeM');
+btnDecode.addEventListener('click', () => {
+  document.getElementById('message_out').innerHTML = `El resultado es: ${"message_in"}`; // colocar variable *textocifrado*
+})
+//función botón "enviar" mensaje, es un boton falso que mostrará otra pantalla
 let btnSend = document.getElementById('sendMessage'); //declarar variable boton enviar
 btnSend.addEventListener('click', () => {
   document.getElementById('scrnNotice').style.display = 'block';
   document.getElementById('scrnPrincipal').style.display = 'none';
+})
+//función botón "volver" mensaje, es un boton que llevará la pantalla de inicio
+let btnBack = document.getElementById('toBack'); //declarar variable boton enviar
+btnBack.addEventListener('click', () => {
+  document.getElementById('scrnNotice').style.display = 'none';
+  document.getElementById('scrnPrincipal').style.display = 'block';
 })
